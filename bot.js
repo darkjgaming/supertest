dfe90b2ef12b34d24d41634f118f21e501d8aa71
darkjgaming <43813784+darkjgaming@users.noreply.github.com>
2018-10-03 10:02:06 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -34,7 +34,7 @@ bot.on('ready', () => {
 });
 bot.on("message", async message => {
     let log = message.guild.channels.find(c => c.name === "test");
-    if (message.channel.id === "405691443961790474") {
+    if (message.channel.id === "215629373049536513") {
         message.delete().catch()
         if (message.content.toLowerCase() === "The first clue here") {
             message.delete().catch()
